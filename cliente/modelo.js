24d51417d712cfc31089450e469ca1c6a14d78d5
TrookeM <migatoyorch@gmail.com>
2024-10-08 18:49:40 +0200

    Funciones básicas añadidas
    También se han añadido datos iniciales
    
    Implementación de los puntos 1.1 a 1.4

---
+++ cliente/modelo.js
@@ -0,0 +1,39 @@
+function Sistema(){
+    this.usuarios = {}; //que tipo coleccion?
+    // Operaciones sobre la colección
+    this.agregarUsuario = function(nick){
+        usr=nick;
+        if(!nick){
+            return false
+        }
+        if (!this.usuarios[nick]){
+            this.usuarios[nick] = new Usuario(nick);
+            return "El usuario " + usr + " ha sido agregado correctamente";
+        }
+        return "El usuario " + usr + " ya existe";
+    }
+    this.eliminarUsuario = function(nick){
+        usr=nick;
+        if (this.usuarios[nick]){
+            delete this.usuarios[nick];
+            return "Usuario " + usr + " eliminado correctamente";
+        }
+        return "El usuario " + usr + " no se pudo encontrar";
+    }
+    this.obtenerUsuarios=function(){
+        return this.usuarios
+    }
+    this.usuarioActivo = function(nick){
+        if (this.usuarios[nick]){
+            return true
+        }
+        return false
+    }
+    this.numeroUsuarios = function() {
+        return Object.keys(this.usuarios).length;
+    };    
+}
+
+function Usuario(nick){
+    this.nick = nick;
+}
